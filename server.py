866745bc60924714c219fbb7bfb2b6041ee82d7f
nikefd <617185699@qq.com>
2015-03-04 14:36:55 +0800

    first post:accomplish the basic communication function

---
+++ server.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python2
+
+import Settings
+import logging
+import tornado.web
+import tornado.httpserver
+import tornado.websocket
+
+#logging.basicConfig(level=logging.INFO,
+#                filename='myapp.log',
+#                filemode='w')
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+	    (r"/websocket", WebSocketHandler),
+        ]
+        settings = {
+            "template_path": Settings.TEMPLATE_PATH,
+            "static_path": Settings.STATIC_PATH,
+        }
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("editor.html")
+
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
+    def on_message(self, message):
+#	logging.debug("This is a debug message")
+#	logging.info("got message %r", message)
+	print "hello"
+	f = open("test.txt", "w")
+	print >> f, message
+	f.close()
+#	self.write_message("this is ") #Sends the given message to the client of this Web Socket.
+
+def main():
+    applicaton = Application()
+    http_server = tornado.httpserver.HTTPServer(applicaton)
+    http_server.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
